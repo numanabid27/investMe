ae50f200f51b36c600dff74f3a53a4b2c07497c1
alirazaztdev1 <aliraza.ztdev1@gmail.com>
2024-08-13 20:27:19 +0500

    update codwe

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ $('.owl-carousel.testimonial').owlCarousel({
             items:1,
         },
         600:{
-            items:1
+            items:2
         },
         1000:{
             items:3
